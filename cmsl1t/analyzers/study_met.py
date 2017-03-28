2d3e9af08b696f6d21b99e4587e6e3d7ab504509
Ben Krikler <b.krikler@cern.ch>
2017-03-28 14:25:37 +0200

    Changes to get first analyzer running ok

--- cmsl1t/analyzers/study_met.py
+++ cmsl1t/analyzers/study_met.py
@@ -1,3 +1,18 @@
 """
 Study the MET distibutions and various PUS schemes
 """
+
+import BaseAnalyzer
+
+class Analyzer(BaseAnalyzer.BaseAnalyzer):
+    def __init__(self):
+        pass
+
+#    def prepare_for_events(self,reader):
+#        pass
+
+    def process_event(self,entry,event):
+        pass
+
+    def make_plots(self):
+        pass
