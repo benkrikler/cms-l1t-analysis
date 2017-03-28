e0facf825ca0b7078af00e66c4c27be437c9647d
Ben Krikler <b.krikler@cern.ch>
2017-03-28 16:46:40 +0200

    Tidy up study_met properly

--- cmsl1t/analyzers/study_met.py
+++ cmsl1t/analyzers/study_met.py
@@ -4,15 +4,14 @@ Study the MET distibutions and various PUS schemes
 
 from BaseAnalyzer import BaseAnalyzer
 
-class Analyzer(BaseAnalyzer.BaseAnalyzer):
-    def __init__(self):
-        pass
-
 class Analyzer(BaseAnalyzer):
     def __init__(self,config):
         super(self,BaseAnalyzer).init("study_met",config)
 
-    def process_event(self,entry,event):
+    def prepare_for_events(self,reader):
+        pass
+
+    def fill_histograms(self,entry,event):
         pass
 
     def make_plots(self):
