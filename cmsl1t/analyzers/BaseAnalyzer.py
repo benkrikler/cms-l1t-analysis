a8ed5144bcfa8448069afa2980445f14648c2335
Ben Krikler <b.krikler@cern.ch>
2017-03-28 14:25:37 +0200

    Changes to get first analyzer running ok

---
+++ cmsl1t/analyzers/BaseAnalyzer.py
@@ -0,0 +1,60 @@
+""" A Base class to be used by the various analyzers"""
+
+import exceptions
+
+class BaseAnalyzer():
+    def __init__():
+        pass
+
+    def prepare_for_events(self,reader):
+        """
+        Can be overloaded in the derived class.
+        Called once, after the input trees have been prepared.
+
+        returns:
+          Should return True if everything was prepared ok.
+          If anything else is returned, processing will stop
+        """
+        return True
+
+    def process_event(self,entry,event):
+        """Should not really be overloaded in the derived class"""
+        return self.fill_histograms(entry,event)
+
+    def fill_histograms(self,entry,event):
+        """ 
+        Has to be overloaded by users code. 
+
+        Called once per input event in the tuples.
+
+        parameters:
+         - entry -- the index of the entry being read in
+         - event -- the event data read in from the tuples
+
+        returns:
+          Should return True if histograms were filled without problem.
+          If anything else is returned, processing of the trees will stop
+        """
+        raise NotImplementedError("fill_histograms needs to be implemented")
+        return True
+
+    def make_plots(self):
+        """ 
+        Has to be overloaded by users code. 
+
+        Called after all events have been read to convert histograms to actual plots
+        Might be called on existing files of histograms (ie. without reading tuples in again)
+
+        returns:
+          Should return True if plots were produced without problem.
+          If anything else is returned, processing of the trees will stop
+        """
+        raise NotImplementedError("make_plots needs to be implemented")
+
+    def finalize(self):
+        """
+        Can be overloaded in the derived class if needed.
+
+        Called at the very end of the code to tidy things up before the programs quits
+        """
+        pass
