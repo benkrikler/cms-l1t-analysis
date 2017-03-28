1568b4b6e281208fc5f09637256f5eb7f7d5ccbd
Ben Krikler <b.krikler@cern.ch>
2017-03-28 17:46:35 +0200

    Implement further the study_met analyzer

--- cmsl1t/analyzers/study_met.py
+++ cmsl1t/analyzers/study_met.py
@@ -3,17 +3,18 @@ Study the MET distibutions and various PUS schemes
 """
 
 from BaseAnalyzer import BaseAnalyzer
-
+from cmsl1t.collections import EfficiencyCollection
 from functools import partial
 import cmsl1t.recalc.met as recalc
+import numpy as np
 
 class Analyzer(BaseAnalyzer):
     def __init__(self,config):
-        super(self,BaseAnalyzer).init("study_met",config)
+        super(Analyzer,self).__init__("study_met",config)
 
         self.met_calcs=dict(
-                RecalcL1EmuMet=dict(title="RE MEt, Barrel",calculate=recalc.met),
-                RecalcL1EmuMetHF=dict(title="RE MEt, Barrel + Fwd",calculate=recalc.met_HF),
+                RecalcL1EmuMetBarrel=dict(title="RE MEt, Barrel",calculate=recalc.l1MetBarrel),
+                RecalcL1EmuMetFull=dict(title="RE MEt, Barrel + Fwd",calculate=recalc.l1MetFull),
             )
 
 #        [
@@ -28,9 +29,11 @@ class Analyzer(BaseAnalyzer):
 #            # 'RecalcL1EmuMetPUSThreshHF',
 #            # 'RecalcL1Met',
 #            # 'RecalcL1Met28Only',
-#        ])
+#        ]
 
     def prepare_for_events(self,reader):
+        bins = np.arange(0, 200, 25)
+        thresholds = [70, 90, 110]
 
         # histograms that look at:
         ## ET distribution vs, tower eta
@@ -44,13 +47,15 @@ class Analyzer(BaseAnalyzer):
     def fill_histograms(self,entry,event):
         pileup = event.nVertex
         if pileup < 5 or not event.passesMETFilter():
-            continue
+            return True
         histograms.set_pileup(pileup)
 
         offlineMetBE = event.sums.caloMetBE
         for name,config in self.met_calcs.items():
-            onlineMet=config['calculate'](event)
+            onlineMet=config['calculate'].calc(event,pileup)
             self.efficiencies[name].fill(offlineMetBE,onlineMet)
+
+        return True
 
     def make_plots(self):
         pass
--- cmsl1t/recalc/met.py
+++ cmsl1t/recalc/met.py
@@ -1,34 +1,51 @@
 import math
 import numpy as np
 
+class recalcMET():
+    def __init__(self,exclude=None,PUS=None,threshold=None):
+        self.__exclude=exclude
+        self.__PUS=PUS
+        self.__threshold=threshold # Could be included into exclude, or even PUS
 
-def recalcMET(caloTowers, exclude=None):
-    metx = []
-    ets = []
-    phis = []
-    for tower in caloTowers:
-        # ieta = tower.ieta
-        if exclude is not None:
-            if exclude(tower):
+        self.metx = 0
+        self.mety = 0
+        self.__update_mag_phi()
+
+    def calc(self,caloTowers,pileup=None):
+        ets = []
+        phis = []
+        for tower in caloTowers:
+            # ieta = tower.ieta
+            if self.__exclude is not None and self.__exclude(tower):
                 continue
-        phi = (math.pi / 36.0) * tower.iphi
-        et = 0.5 * tower.iet
-        ets.append(et)
-        phis.append(phi)
-    metx = -sum(ets * np.cos(phis))
-    mety = -sum(ets * np.sin(phis))
-    return (metx, mety)
+            phi = (math.pi / 36.0) * tower.iphi
+            et = 0.5 * tower.iet
+            if self.__threshold is not None and et < self.__threshold:
+                continue
+            if self.__PUS is not None:
+                et= self.__PUS(tower,pileup)
+            ets.append(et)
+            phis.append(phi)
+        self.metx = -sum(ets * np.cos(phis))
+        self.mety = -sum(ets * np.sin(phis))
+        self.__update_mag_phi()
 
+    def __update_mag_phi(self):
+        self.mag = math.sqrt(self.metx**2 + self.mety**2)
+        self.phi= math.atan2(self.mety,self.metx)
+        # TODO: Should these members be read-only properties?
+
+def l1MetBarrel(caloTowers):
+    return recalcMET(caloTowers, exclude=lambda tower: abs(tower.ieta) <= 28)
+
+def l1MetFull(caloTowers):
+    return recalcMET(caloTowers)
 
 def l1Met28Only(caloTowers):
-    return recalcMET(caloTowers, exclude=lambda tower: not abs(tower.ieta) == 28)
+    return recalcMET(caloTowers, exclude=lambda tower: abs(tower.ieta) == 28)
 
-# TODO: find better name
-
-
-def l1MetNot28(caloTowers):
-    return recalcMET(caloTowers, exclude=lambda tower: abs(tower.ieta) >= 28)
-
+def l1MetBarrelNot28(caloTowers):
+    return recalcMET(caloTowers, exclude=lambda tower: abs(tower.ieta) < 28)
 
 def l1MetNot28HF(caloTowers):
-    return recalcMET(caloTowers, exclude=lambda tower: abs(tower.ieta) == 28)
+    return recalcMET(caloTowers, exclude=lambda tower: abs(tower.ieta) != 28)
